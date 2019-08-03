983fc0a783c74c1651c1edcb8abfcd8139eba9bd
ZhengMaster2020 <2105195274@qq.com>
2019-08-03 23:51:30 +0800

    update index message and layout

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,61 @@
           <i class="el-icon-menu" @click="isCollapse"></i>
         </div>
       </el-header>
-      <el-aside width="180">
-        <el-menu
-          default-active=""
-          class="el-menu-vertical-menu"
-          mode="vertical"
-          :collapse="collapse"
-          router="true"
-        >
-          <el-submenu index="1" >
-            <template slot="title">
-              <i class="el-icon-s-home"></i>
-              <span slot="title">首页</span>
-            </template>
-            <el-menu-item-group title="Admin">
-              <span slot="title" class>管理员</span>
-              <el-menu-item index="1-1">查看信息</el-menu-item>
-              <el-menu-item index="1-2">修改密码</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-menu-item index="add">
-            <router-link to=/add class="el-icon-circle-plus-outline"></router-link>
-            <router-link slot="title" class="menu-link" to="/add">添加用户</router-link>
-          </el-menu-item>
-          <el-menu-item index="delete">
-            <router-link to="/delete" class="el-icon-remove-outline"></router-link>
-            <router-link slot="title" class="menu-link" to="/delete">删除用户</router-link>
-          </el-menu-item>
-          <el-menu-item index="update">
-            <router-link to="/update" class="el-icon-refresh"></router-link>
-            <router-link slot="title" class="menu-link" to="/update">更新用户</router-link>
-          </el-menu-item>
-          <el-menu-item index="serach">
-            <router-link to="/search" class="el-icon-search"></router-link>
-            <router-link slot="title" class="menu-link" to="/search" >查找用户</router-link>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
-      <el-main><router-view></router-view></el-main>
+      <el-row>
+        <el-col :span="asieSpan">
+          <el-aside>
+            <el-menu
+              default-active="/"
+              class="el-menu-vertical-menu"
+              mode="vertical"
+              :collapse="collapse"
+              :router="true"
+            >
+              <el-menu-item index="/">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">首页</span>
+              </el-menu-item>
+
+              <el-menu-item index="/add">
+                <i class="el-icon-circle-plus-outline"></i>
+                <span slot="title">添加用户</span>
+              </el-menu-item>
+
+              <el-menu-item index="/delete">
+                <i class="el-icon-remove-outline"></i>
+                <span slot="title">删除用户</span>
+              </el-menu-item>
+
+              <el-menu-item index="/update">
+                <i class="el-icon-refresh"></i>
+                <span slot="title">更新用户</span>
+              </el-menu-item>
+
+              <el-menu-item index="/search">
+                <i class="el-icon-search"></i>
+                <span slot="title">查找用户</span>
+              </el-menu-item>
+
+              <el-submenu index="/admin">
+                <template slot="title">
+                  <i class="el-icon-user"></i>
+                  <span slot="title">管理员</span>
+                </template>
+                <el-menu-item-group title="Admin">
+                  <span slot="title" class>管理员</span>
+                  <el-menu-item index="/admin/msg">查看信息</el-menu-item>
+                  <el-menu-item index="/admin/update">修改密码</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+            </el-menu>
+          </el-aside>
+        </el-col>
+        <el-col :span="mainSpan" class="main">
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-col>
+      </el-row>
     </el-container>
   </div>
 </template>
@@ -51,51 +68,50 @@
 <script>
 export default {
   name: "app",
-  data () {
+  data() {
     return {
-      collapse: false
-    }
+      collapse: false,
+      asieSpan: 3,
+      mainSpan: 21
+    };
   },
   components: {},
   methods: {
-    isCollapse () {
-      this.collapse = !this.collapse
+    isCollapse() {
+      this.collapse = !this.collapse;
+      if (this.collapse === true) {
+        this.asieSpan = 1;
+        this.mainSpan = 23;
+      } else {
+        this.asieSpan = 3;
+        this.mainSpan = 21;
+      }
     }
   }
-}
+};
 </script>
 
 <style>
-.menu-link{
-  color: #303133;
-}
-.router-link-active{
-  color: #409EFF;
-}
 .el-header {
   background-color: cadetblue;
   padding: 0;
   position: relative;
 }
-.header-logo{
+.header-logo {
   margin-left: 16px;
   position: absolute;
-  top:50%;
+  top: 50%;
   transform: translate(0, -50%);
   color: #fff;
 }
 .header-logo i {
   font-size: 30px;
 }
-/* .el-menu>li,a {
-  columns: #551a8b ;
-} */
 .el-main {
-  float: right;
-  background: green;
+  background: #cccccc26;
 }
 .el-menu-vertical-menu:not(.el-menu--collapse) {
-  width: 180px;
+  width: 189px;
   min-height: 400px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,88 @@
 <template>
   <div>
-    Home
+    <el-table :data="tableData" :stripe="true" :highlight-current-row="true" >
+      <el-table-column prop="date" label="日期">
+        <template>
+          <i class="el-icon-time"></i>
+          <span></span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="birthplace" label="籍贯"></el-table-column>
+      <el-table-column prop="number" label="号码"></el-table-column>
+      <el-table-column prop="education" label="学历"></el-table-column>
+      <el-table-column prop="position" label="职位"></el-table-column>
+      <el-table-column label="操作">
+        <el-button-group>
+          <el-button type="primary" size="small" :circle="true">详情</el-button>
+          <el-button type="danger" size="mini" :circle="true">删除</el-button>
+        </el-button-group>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  data() {
+    return {
+      tableData: [
+        {
+          date: new Date().toLocaleDateString(),
+          name: "张三",
+          birthplace: "广东广州",
+          number: "18998555668",
+          education: "本科",
+          position: "前端工程师"
+        },
+        {
+          date: new Date().toLocaleDateString(),
+          name: "张四",
+          birthplace: "广东深圳",
+          number: "189985556789",
+          education: "专科",
+          position: "后端工程师"
+        },
+        {
+          date: new Date().toLocaleDateString(),
+          name: "李四",
+          birthplace: "广东茂名",
+          number: "13498555668",
+          education: "研究生",
+          position: "Java工程师"
+        },
+        {
+          date: new Date().toLocaleDateString(),
+          name: "王五",
+          birthplace: "广东珠海",
+          number: "18999555668",
+          education: "本博士科",
+          position: "H5工程师"
+        },
+        {
+          date: new Date().toLocaleDateString(),
+          name: "苟六",
+          birthplace: "广东东莞",
+          number: "13415645796",
+          education: "博士后",
+          position: "人工智能"
+        },{
+          date: new Date().toLocaleDateString(),
+          name: "张良",
+          birthplace: "四川成都",
+          number: "13415645886",
+          education: "讲师",
+          position: "AI人脸识别"
+        },
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.el-button{
+  padding: 10px;
+}
 </style>
